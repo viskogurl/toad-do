93d0d96179a122c5b1f975f64a1202917c488f63
Ian King <ianking741@gmail.com>
2021-06-25 20:19:56 -0500

    Renamed 'Tryify' to 'Klar' and added Throwify function.

---
+++ utils/klar.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Welcome to the meat and bones of Toad-Do.
+//
+// Klar (German for clean or crisp) is a simple utils library
+// meant to replace repetitive code blocks with simple functions.
+//
+// In 15 lines of code, Klar gets rid of the need for try-catching 
+// asyncronous function by returning the Promise data and error in 
+// and Array.
+
+const tryify = async (promise) => {
+    try {
+        const data = await promise;
+        return [data, null];
+    } catch (err) {
+        return [null, err];
+    }
+}
+
+const throwify = (error) => {
+    throw new Error(error);
+}
+
+module.exports = { tryify, throwify };
