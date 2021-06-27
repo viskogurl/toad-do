42269e130bb558a1131c888ce028ce23e1e154b2
Ian King <ianking741@gmail.com>
2021-06-26 23:02:40 -0500

    Added color checking middleware.

---
+++ middleware/colorChecker.js
@@ -0,0 +1,15 @@
+module.exports = colorChecker = () => {
+  return colorChecker = (req, res, next) => {
+    const hexColorMap = {
+      'red': '#ff0000',
+      'green': '#00ff00',
+      'blue': '#0000ff',
+    };
+
+    if (req.body.color) {
+      const color = req.body.color;
+      req.body.color = hexColorMap[color.toLowerCase()] ?? '#000000';
+    } 
+    next();
+  };
+};
